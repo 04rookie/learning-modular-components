781af2f9034752a488bf658c2d0d54216018852b
04rookie <47923475+04rookie@users.noreply.github.com>
2021-02-08 15:50:21 +0530

    Initial commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading.jsx";
+import UnorderedList from "./UnorderedList";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <UnorderedList />
+    </div>
+  );
+}
+
+export default App;
